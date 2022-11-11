e00a11a6d9ee3eda0b1cf9f3281e9393afddb796
Essenbay Assel <esenbay.asel@mail.ru>
2022-11-11 20:35:24 +0600

    modified:   src/components/CarPlace/Car.css 	modified:   src/components/CarPlace/Car.jsx 	modified:   src/components/Road/Road.css 	modified:   src/components/Road/Road.jsx

--- src/components/CarPlace/Car.jsx
+++ src/components/CarPlace/Car.jsx
@@ -1,10 +1,24 @@
 import './Car.css'
 import React from 'react'
+import carImage from '../../images/car.png'
 
-function Car({location}){
+function Car({car}){
+    const carStyle = {
+
+    }
+        
+    const getRotation = ()=> {
+        switch(car.location){
+            case "horizonral-upper": break;
+            case "horizonral-down": break;
+            case "vertical-left": break;
+            case "vertical-right": break;
+            default: console.log("Couldn't find location")
+        }
+    }
     return(
         <div className="car">
-            
+           {/* <img src={carImage} alt="Car"/> */}
         </div>
     )
 }
--- src/components/Road/Road.jsx
+++ src/components/Road/Road.jsx
@@ -1,15 +1,24 @@
 import './Road.css'
 import React, { useState } from 'react'
+import Car from '../CarPlace/Car';
 
 function Road(){
     const [cars, setCars] = useState([]);
+    const [upperCars, setUpperCars] = useState([]);
+    const [downCars, setDownCars] = useState([]);
+    const [leftCars, setLeftCars] = useState([]);
+    const [rightCars, setRightCars] = useState([]);
     const addCar = (location) => {
-        if(location){ //check if they are not empty
-            const newCar = {
-                id: Math.random().toString(36).substr(2,9),
-                location: location
-            }
-            setCars([...cars, newCar]);
+        const newCar = {
+            id: Math.random().toString(36).substr(2,9),
+            location: location
+        }
+        switch(location){
+            case "horizonral-upper": setUpperCars([...upperCars, newCar]); break;
+            case "horizonral-down": setDownCars([...downCars, newCar]); break;
+            case "vertical-left": setLeftCars([...leftCars, newCar]); break;
+            case "vertical-right": setRightCars([...rightCars, newCar]); break;
+            default: console.log("Couldn't find location")
         }
         console.log(`Added car in ${location}`)
     }
@@ -18,23 +27,35 @@ function Road(){
         <div className="road">
             <div className="road-horizontal">
                 <div className="road-upper lane-group">
-                    <div className="lane-1" onClick={() => {addCar("horizonral-upper-1")}}>Lane 1</div>
-                    <div className="lane-2" onClick={() => {addCar("horizonral-upper-2")}}>Lane 2</div>
+                    <div className="lane-1 lane" onClick={() => {addCar("horizonral-upper")}}>
+                        <div className="car-item">
+                            Car
+                        </div>
+                        {/* {upperCars.map((car) =>{
+                            return (
+                                <Car 
+                                    key={car.id}
+                                    car={car}
+                                />
+                            )
+                        })} */}
+                    </div>
+                    <div className="lane-2 lane" onClick={() => {addCar("horizonral-upper")}}>Lane 2</div>
                 </div> 
                 <div className="road-down lane-group">
-                    <div className="lane-1" onClick={() => {addCar("horizonral-upper-1")}}>Lane 1</div>
-                    <div className="lane-2" onClick={() => {addCar("horizonral-upper-2")}}>Lane 2</div>
+                    <div className="lane-1 lane" onClick={() => {addCar("horizonral-upper")}}>Lane 1</div>
+                    <div className="lane-2 lane" onClick={() => {addCar("horizonral-upper")}}>Lane 2</div>
                 </div> 
             </div>
 
             <div className="road-vertical">
                 <div className="road-left lane-group">
-                    <div className="lane-1" onClick={() => {addCar("vertical-left-1")}}>Lane 1</div>
-                    <div className="lane-2" onClick={() => {addCar("vertical-left-2")}}>Lane 2</div>
+                    <div className="lane-1 lane" onClick={() => {addCar("vertical-left")}}>Lane 1</div>
+                    <div className="lane-2 lane" onClick={() => {addCar("vertical-left")}}>Lane 2</div>
                 </div> 
                 <div className="road-right lane-group">
-                    <div className="lane-1" onClick={() => {addCar("vertical-right-1")}}>Lane 1</div>
-                    <div className="lane-2" onClick={() => {addCar("vertical-right-2")}}>Lane 2</div>
+                    <div className="lane-1 lane" onClick={() => {addCar("vertical-right")}}>Lane 1</div>
+                    <div className="lane-2 lane" onClick={() => {addCar("vertical-right")}}>Lane 2</div>
                 </div> 
             </div>
         </div>
